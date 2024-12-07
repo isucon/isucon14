ea58c0a48bcd70a9cfe8f5bcdbd5607009eea549
ryoha000 <the7uya@gmail.com>
2024-12-07 18:32:34 +0900

    dbg

--- bench/payment/handler.go
+++ bench/payment/handler.go
@@ -70,7 +70,7 @@ func (s *Server) PostPaymentsHandler(w http.ResponseWriter, r *http.Request) {
 
 	failureCount, _ := s.failureCounts.GetOrSetDefault(token, func() int { return 0 })
 	if p.locked.CompareAndSwap(false, true) {
-		if failureCount >= 4 {
+		if failureCount >= 0 {
 			alreadyProcessed := false
 			if !newPayment {
 				alreadyProcessedIdks := map[string]struct{}{}
