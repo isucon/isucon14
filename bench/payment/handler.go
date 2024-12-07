1c852b9ce7a7b1eaac1ebb56db2b408ada7b8ec9
ryoha000 <the7uya@gmail.com>
2024-12-07 19:52:36 +0900

    fix

--- bench/payment/handler.go
+++ bench/payment/handler.go
@@ -61,6 +61,8 @@ func (s *Server) PostPaymentsHandler(w http.ResponseWriter, r *http.Request) {
 			writeJSON(w, http.StatusUnprocessableEntity, map[string]string{"message": "リクエストペイロードがサーバーに記録されているものと異なります"})
 			return
 		}
+		writeResponse(w, p.Status)
+		return
 	} else {
 		p.Token = token
 		p.Amount = req.Amount
